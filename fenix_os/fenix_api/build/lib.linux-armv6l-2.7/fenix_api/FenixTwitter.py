485451c57cf789f6d400074af9b53231766ebf13
aridet <aridet@localhost.localdomain>
2019-05-31 15:03:06 +0000

    fix(): fix security

--- fenix_os/fenix_api/build/lib.linux-armv6l-2.7/fenix_api/FenixTwitter.py
+++ fenix_os/fenix_api/build/lib.linux-armv6l-2.7/fenix_api/FenixTwitter.py
@@ -12,10 +12,10 @@ class FenixTwitter:
         self.Main()
 
     def Main(self):
-        consumer_key = 'jaL8cPgN86MiXI0tCLYEzC1xc'
-        consumer_secret = 'okaIFI0m3dayTAk2w2qvjWJYjkNFkXxXzVzU9H9Pfmo47K5PZw'
-        access_token_key = '4177870763-fOib4cgEjTbenGGAI4i3UeziszPiaUIrnSsiN4v'
-        access_token_secret = 'h185EIXJY37Ex7LM5OR9dsrS9XLSV8ehrgRHZb67M5Hpp'
+        consumer_key = ''
+        consumer_secret = ''
+        access_token_key = ''
+        access_token_secret = ''
         self.api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
     
     def Request(self):
--- fenix_os/fenix_api/build/lib.linux-armv7l-2.7/fenix_api/FenixTwitter.py
+++ fenix_os/fenix_api/build/lib.linux-armv7l-2.7/fenix_api/FenixTwitter.py
@@ -12,10 +12,10 @@ class FenixTwitter:
         self.Main()
 
     def Main(self):
-        consumer_key = 'ZRgXWodHhxDvySQ6hrp9K5LKm'
-        consumer_secret = 'y8Tj7mx3LKsmnoUom5niJKWD4OOk5x7Up5XUUDj1kUjZsoibFx'
-        access_token_key = '841412456769552386-Dcqma3HyLMo0JYRHO82yP6kl5Q5Yknb'
-        access_token_secret = 'OVnwSaKlpLizTp4e3KOfnpCrb93Df7enh2sosGDfJa3q9'
+        consumer_key = ''
+        consumer_secret = ''
+        access_token_key = ''
+        access_token_secret = ''
         self.api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
     
     def Request(self):
